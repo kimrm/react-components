7a135a43d43bf42897ca854ec4c3e0a91ee5431a
Kim Rune Møller <kimrune@hotmail.no>
2024-11-12 01:14:03 +0100

    demo component

---
+++ src/components/ImageGalleryDemo.tsx
@@ -0,0 +1,39 @@
+import ImageGallery from "./imageGallery";
+
+export default function ImageGalleryDemo() {
+  return (
+    <>
+      <ImageGallery
+        config={{
+          closeButtonLabel: "Close dialog"
+        }}
+      >
+        <img
+          src="/public/dummy_600x400_ffffff_cccccc.png"
+          alt="Placeholder 1"
+          title="Awesome title"
+        />
+        <img
+          src="/public/dummy_600x400_ffffff_cccccc.png"
+          alt="Placeholder 2"
+          title="Awesome title"
+        />
+        <img
+          src="/public/dummy_600x400_ffffff_cccccc.png"
+          alt="Placeholder 3"
+          title="Awesome title"
+        />
+        <img
+          src="/public/dummy_600x400_ffffff_cccccc.png"
+          alt="Placeholder 4"
+          title="Awesome title"
+        />
+        <img
+          src="/public/dummy_600x400_ffffff_cccccc.png"
+          alt="Placeholder 5"
+          title="Awesome title"
+        />
+      </ImageGallery>
+    </>
+  );
+}
