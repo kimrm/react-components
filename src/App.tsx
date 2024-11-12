44316ab41b257c61292d9aae3cd63d5c09372677
Kim Rune Møller <kimrune@hotmail.no>
2024-11-12 14:59:51 +0100

    adds side nav, heading paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,35 @@ import "./App.css";
 import { FlowDemo } from "./components/FlowDemo";
 import ImageGalleryDemo from "./components/ImageGalleryDemo";
 import { TabDemo } from "./components/TabDemo";
+import SideNavigation from "./components/ui/sideNavigation";
 
 function App() {
   return (
-    <>
-      <h1>Components library</h1>
-      <section>
-        <h2>Simple Flow Component</h2>
-        <FlowDemo />
-      </section>
-      <section>
-        <h2>Simple Tab Component</h2>
-        <TabDemo />
-      </section>
-      <section>
-        <h2>Simple Image Gallery Component</h2>
-        <p>Work in progress</p>
-        <ImageGalleryDemo />
-      </section>
-    </>
+    <div className="app-container">
+      <div>
+        <SideNavigation />
+      </div>
+      <div>
+        <h1>Components library</h1>
+        <p>
+          This is just a personal collection of React components I&apos;m
+          creating, where I try out new things.
+        </p>
+        <section id="flow">
+          <h2>Simple Flow Component</h2>
+          <FlowDemo />
+        </section>
+        <section id="tab">
+          <h2>Simple Tab Component</h2>
+          <TabDemo />
+        </section>
+        <section id="gallery">
+          <h2>Simple Image Gallery Component</h2>
+          <p>Work in progress</p>
+          <ImageGalleryDemo />
+        </section>
+      </div>
+    </div>
   );
 }
 
